6f1158f44f5f1d5d76861fc77750103f2d021674
magicprem <magicprem@gmail.com>
2025-06-05 10:33:18 +0530

    Update A.java

--- src/com/test/A.java
+++ src/com/test/A.java
@@ -4,7 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
-		System.out.println("now kumar has done some changes" + "yes");
+		System.out.println("now prem has done some changes" + "yes");
 	}
 
 }
